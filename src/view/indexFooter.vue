6f6c30eeee15c4007e84704335d8c93d3490ea52
zhigang <codcoe@foxmail.com>
2018-10-09 16:45:52 +0800

    首页

--- src/view/indexFooter.vue
+++ src/view/indexFooter.vue
@@ -1,73 +1,37 @@
 <template>
-    <div class="footer_wrap">
-        <div class="left fl">
-            <h1 class="mb15 logo">2KEX Global</h1>
-            <p class="mb15">全球领先的数字资产交易平台</p>
-            <div class="icon_img">
-                <i class="iconfont icon-weibo"></i>
-                <i class="iconfont icon-weibo"></i>
-                <i class="iconfont icon-weibo"></i>
-                <i class="iconfont icon-weibo"></i>
-            </div>
-            <p class="mt50">© 2013-2018 2KEX Global</p>
+    <div id="footer">
+        <div>
+            <span>联系我们</span>
+            <span>费率标准</span>
+            <span>API文档</span>
+            <span>用户协议</span>
+            <span>本站公告</span>
         </div>
-        <div class="fr">
-            <ul>
-                <li class="pb50">
-                    <p class="fl tl foot_title ft12" v-for="item in titleList">{{item}}</p>
-                </li>
-                <li v-for="item in conList"><a class="con_a mb15 ft16" v-for="itm in item.list" v-bind:href="itm.src">{{itm.text}}</a></li>
-            </ul>
-        </div>
+        <p class="">© 2013-2018 环球数贸信息平台</p>
     </div>
 </template>
+
 <script>
 export default {
-  data() {
-    return {
-      titleList: ["服务", "工具", "关于", "条款说明"],
-      conList: [
-        {
-          list: [{ text: "2KEX咨询", src: "" },{ text: "客户端下载", src: "" },{ text: "2KEX集团", src: "" },{ text: "用户协议", src: "" }]
-        },
-        {
-          list: [{ text: "2KEX矿池", src: "" },{ text: "帮助中心", src: "" },{ text: "联系我们", src: "" },{ text: "隐私条款", src: "" }]
-        },
-        {
-          list: [{ text: "2KEX生态", src: "" },{ text: "API文档", src: "" },{ text: "加入我们", src: "" },{ text: "法律声明", src: "" }]
-        },
-        {
-          list: [{ text: "2KEX资本", src: "" },{ text: "数字资产介绍", src: "" },{ text: "上币申请", src: "" },{ text: "费率", src: "" }]
-        },
-        {
-          list: [{ text: "机构账户", src: "" },{ text: "新手指导", src: "" },{ text: "公告", src: "" }]
-        }
-      ]
-    };
-  }
-};
+
+}
 </script>
-<style scoped>
-.footer_wrap {
-  background: #09162e;
-  color: #6b80ae;
-  overflow: hidden;
-  padding: 50px 80px;
-}
-.foot_title,.con_a{
-  display: inline-block;
-  width: 180px;
-  text-align: left;
-}
-.con_a{
-    color: #aabdbc;
-}
-.con_a:hover{
-    color: #d45858;
-}
-.logo{
-    color: #6b80ae;
+
+<style lang='scss'>
+#footer{
+    background: #09162e;
+    color:#aabdbc;
+    font-size: 14px;
+    text-align: center;
+    >div{
+        padding: 20px;
+        span{
+            margin-right: 30px;cursor: pointer;
+            &:hover{color:#d45858}
+        }
+    }
+    p{
+        padding: 10px;
+    }
 }
 </style>
-
-
